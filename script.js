a2268021d3eb6f9444b32fb30c5064d522377315
Levente Nagy <113554470+Levkol@users.noreply.github.com>
2023-06-16 07:16:13 +0200

    3, 4

--- script.js
+++ script.js
@@ -430,7 +430,7 @@ Then, call the function again with players from game.scored
 
 GOOD LUCK 😀
 */
-
+/*
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -471,7 +471,7 @@ const game = {
     team2: 6.5,
   },
 };
-
+*/
 /*
 const [player1, player2] = game.players;
 
@@ -518,7 +518,7 @@ Get the team names directly from the game object, don't hardcode them (except fo
   }
 
 GOOD LUCK 😀
-*/
+
 
 for (const [i, player] of game.scored.entries()) {
   console.log(`Goal ${i + 1}: ${player}`);
@@ -539,3 +539,104 @@ for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === `x` ? `draw` : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr} ${odd}`);
 }
+*/
+
+/*
+Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map called 'gameEvents' (see below) with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+
+GOOD LUCK 😀
+*/
+/*
+const gameEvents = new Map([
+  [17, '⚽ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+console.log(
+  `An event happened, on average, every ${92 / gameEvents.size} minutes.`
+);
+
+// for (const [key, value] of gameEvents) {
+//   if (key < 45) {
+//     console.log(`[FIRST HALF] ${key}: ${value}`);
+//   } else if (key > 45) {
+//     console.log(`[SECOND HALF] ${key}: ${value}`);
+//   }
+// }
+
+for (const [key, value] of gameEvents) {
+  console.log(
+    `${key > 45 ? `[FIRST HALF]` : `[SECOND HALF]`} ${key}: ${value}`
+  );
+}
+*/
+
+/*
+Coding Challenge #4
+
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to insert the elements), and conversion will happen when the button is pressed.
+
+Test data (pasted to textarea, including spaces):
+underscore_case
+ first_name
+Some_Variable
+  calculate_AGE
+delayed_departure
+
+Should produce this output (5 separate console.log outputs):
+underscoreCase     ✅
+firstName          ✅✅
+someVariable       ✅✅✅
+calculateAge       ✅✅✅✅
+delayedDeparture   ✅✅✅✅✅
+
+Hints:
+§ Remember which character defines a new line in the textarea 😉
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable
+name conversion working 😉
+§ This challenge is difficult on purpose, so start watching the solution in case
+you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+
+*/
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector(`button`).addEventListener(`click`, function () {
+  const text = document.querySelector(`textarea`).value;
+  const rows = text.split(`\n`);
+
+  for (const [i, value] of rows.entries()) {
+    const [first, second] = value.trim().toLowerCase().split(`_`);
+    const output = `${first}${second[0].toUpperCase() + second.slice(1)}`;
+    console.log(`${output.padEnd(20)} ${`✅`.repeat(i + 1)}`);
+  }
+});
